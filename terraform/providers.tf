82571fd40f4f9fdfecab77c039f9c0eb6cea0423
ManindraG59 <manindragandham59@gmail.com>
2025-08-16 15:52:02 +0530

    add terraform pipeline and remote backend configs

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -7,6 +7,13 @@ terraform {
       version = "~> 4.40.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "Sourin-Alu-Candidate"
+    storage_account_name = "tfstateacct"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
